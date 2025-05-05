48ea9d3caf0ec4c8af33f3a4c6b56f82aced8bf2
Najmaldin Ishag <najmaldinosman@gmail.com>
2025-05-05 06:09:18 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import AppSidebar from "@/components/AppSidebar";
+import NavBar from "@/components/ui/NavBar";
+import { ThemeProvider } from "@/components/Providers/ThemeProviders";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +26,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <AppSidebar />
+          <main className="w-full">
+            <NavBar />
+          </main>
+          <div className="px-4">{children}</div>
+        </ThemeProvider>
       </body>
     </html>
   );
