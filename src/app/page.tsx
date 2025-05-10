b205833a4821bde0208cde3caf4a7636cd57cc71
Najmaldin Ishag <najmaldinosman@gmail.com>
2025-05-10 23:55:16 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import AppAreaChart from "@/components/AppAreaChart";
 import AppBarChart from "@/components/AppBarChart";
 import AppPieChart from "@/components/AppPieChart";
 import CardList from "@/components/CardList";
+import TodoList from "@/components/TodoList";
 
 const Homepage = () => {
   return (
@@ -19,7 +20,7 @@ const Homepage = () => {
         <AppPieChart />
       </div>
       <div className="bg-primary-foreground p-4 rounded-lg">
-        {/* <TodoList /> */} Test
+        <TodoList />
       </div>
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
         <AppAreaChart />
